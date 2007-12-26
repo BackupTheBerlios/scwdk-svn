63bb371c5bfe1ee1f372b384db76b625def95e43
bretzel <bretzel@22dbb4d9-7441-0410-ab70-fb548e8bd9cd>
2007-12-26 12:38:17 +0000

    For notes see the ones for swmain.h

--- trunk/library/swmain.cpp
+++ trunk/library/swmain.cpp
@@ -26,6 +26,7 @@ swMain::swMain()
  : swObject()
 {
     _Self = this;
+    _evq_x = 0l;
 }
 
 
@@ -33,7 +34,7 @@ swMain::swMain( const std::string& _name, int argc, char** argv ): swObject( 0l,
 {
     _Self = this;
     for( int i=0; i<argc; i++ ) _args.push_back( argv[i] );
-    
+    _evq_x = 0l;
 }
 
 
@@ -45,7 +46,7 @@ swMain::~swMain()
         delete _nc;
     }
     if(_dsk) delete _dsk;
-
+    delete _evq_x;
 }
 
 
@@ -73,6 +74,110 @@ bool swMain::QueryExit(swObject* _sender )
 
 
 /*!
+    \fn swMain::PostEvent( Event* _e )
+    
+ */
+int swMain::PostEvent( Event* _e )
+{
+    /// @todo Implement threading protection ( mutex )
+    _evq_x->lock();
+    _evq.push_back( _e );
+    _evq_x->unlock();
+    return _evq.size();
+    
+}
+
+
+/*!
+    \fn swMain::_prepare_mouse_ev( int nc )
+ */
+Event* swMain::_prepare_mouse_ev( int nc )
+{
+    MEVENT mev;
+    if(getmouse( &mev ) == ERR ) return 0l;
+    MouseEvent* Mev = new MouseEvent( mev );
+    return mev;
+}
+
+/*!
+    \fn swMain::_prepare_keyinput_ev( Event* _ev )
+ */
+Event* swMain::_prepare_keyinput_ev( Event* _ev )
+{
+    event_t ev = 0;
+    NMessageEvent* Mes;
+    KeyPressEvent* Kev;
+    if( e == KEY_RESIZE){
+        PostEvent( (Mes = new MessageEvent(event::TermResize) ) );
+        return Mes;
+    }
+    PostEvent( ( Kev = new NKeyPressEvent( e, bMeta ) ) );
+    bMeta = false;    /// @todo implement me
+    return  Kev;
+}
+
+/*!
+    \fn swMain::_prepare_message_ev(int nc )
+ */
+Event* swMain::_prepare_message_ev(int nc )
+{
+    /// @todo implement me
+}
+
+/*!
+    \fn swMain::_preProcessEvent( Event* _ev )
+ */
+Event* swMain::_preProcessEvent( Event* _ev )
+{
+    int nc = _ev->NcursesEvent();
+    MessageEvent* me;
+    Event* E;
+    if( _ev == ERR ) return ;
+    // is mouse event ?
+    switch( nc ){
+        case KEY_RESIZE:
+            me = new MessageEvent(event::TermResize);
+            E = me;
+            break;
+        case KEY_MOUSE:
+            E = _prepare_mouse_ev ( nc );
+        break;
+//        case KEY_MESSAGE:
+//            E = _prepare_message_ev( nc );
+//        break;
+        default: // Assume keyboard input key event
+            //cerr << _HERE_ << "key even- value=" << __nce << "[" << (char)__nce << ']' <<   endl;
+            E = _prepare_keyinput_ev (  );
+        break;
+    }
+    // delete original Event instance.
+    delete _ev;
+    // Return resulting final Event may be 0L (if event is invalid or not to be propagated)
+    return E;
+}
+
+/*!
+    \fn swMain::DispatchEvents()
+ */
+Event* swMain::DispatchEvents()
+{
+    // no need to lock the queu since new events can be generated from within this thread and from other thread and PostEvent does protect the queu
+    _evq_x->lock();
+        int S = _evq.size();
+    _evq_x->unlock();
+    if(!S) return 0l;
+    Event::iterator i;
+    Event* _e;
+    // Iterate all the events in the queu as well as the ones inserted during the loop...
+    for(; i!= _evq.end(); i++){
+        // Last message may be Event::Quit that was inserted at the end of the queu after all the other messages
+        _e = ProcessEvent( *i );
+        if( _e->What() == Event::Quit ) break; // cancel the loop and process the quit event, if cancelled, inserted events after quit will then be processed otherwize, the app quits!
+    }
+    return _e;
+}
+
+/*!
     \fn swMain::Run()
     \brief Setup the program's environment as the main thread, waits for user/system input events, then
     dispatch it until the event::Quit is the only remaining event to proceed.
@@ -82,43 +187,19 @@ int swMain::Run()
     Event* e;
     Debug << "Initializing...";
     if( (r = Init()) ) return r;
-    // nothing else to do for now  - just test queryexit
+
     do{
         _dsk->Refresh();
         e = _nc->WaitEvent();
         ///@todo Process the event
-        e = _preProcessEvent( e );
+        if( (e = _preProcessEvent( e )) ){
+            PostEvent( e );
+        }
         e = DispatchEvents();
     }while( e->What() != event::Quit );
     return 0;
 }
 
-/*!
-    \fn ncdk::NApplication::translate_ncurses_event ( int )
-    \brief Identify the event code from the ncurses::getch() and then translate the event to the ncdk events engine.
-
-void NApplication::translate_ncurses_event ( int __nce )
-{
-   if( __nce == ERR ) return ;
-   // is mouse event ?
-   switch( __nce ){
-      case KEY_RESIZE:
-         PostEvent( new NMessageEvent(event::TermResize) );
-         break;
-      case KEY_MOUSE:
-         cerr << _HERE_ << "Mouse event " << endl;
-         translate_mouse_ev ( __nce );
-      break;
-      case KEY_MESSAGE:
-         translate_message_ev( __nce );
-      break;
-      default: // Assume keyboard input key event
-         //cerr << _HERE_ << "key even- value=" << __nce << "[" << (char)__nce << ']' <<   endl;
-         translate_keyinput_ev ( __nce );
-      break;
-   }
-}
-*/
 
 /*!
     \fn swMain::Init()
@@ -154,70 +235,4 @@ int swMain::RunOptions()
     return 0; // test
 }
 
-
-/*!
-    \fn swMain::_preProcessEvent( Event* _ev )
- */
-Event* swMain::_preProcessEvent( Event* _ev )
-{
-    int nc = _ev->NcursesEvent();
-    if( _ev == ERR ) return ;
-    // is mouse event ?
-    switch( nc ){
-        case KEY_RESIZE:
-            PostEvent( new NMessageEvent(event::TermResize) );
-            break;
-        case KEY_MOUSE:
-            _prepare_mouse_ev ( nc );
-        break;
-        case KEY_MESSAGE:
-            _prepare_message_ev( nc );
-        break;
-        default: // Assume keyboard input key event
-            //cerr << _HERE_ << "key even- value=" << __nce << "[" << (char)__nce << ']' <<   endl;
-            _prepare_keyinput_ev (  );
-        break;
-    }
-    delete _ev;
-    return 0l;
-}
-
-
-/*!
-    \fn swMain::_prepare_mouse_ev( int nc )
- */
-Event* swMain::_prepare_mouse_ev( int nc )
-{
-    MEVENT mev;
-    getmouse( &mev );
-    MouseEvent* Mev = new MouseEvent( mev );
-    
-}
-
-
-/*!
-    \fn swMain::_prepare_keyinput_ev( Event* _ev )
- */
-Event* swMain::_prepare_keyinput_ev( Event* _ev )
-{
-    event_t ev = 0;
-    NMessageEvent* Mes;
-    KeyPressEvent* Kev;
-    if( e == KEY_RESIZE){
-        PostEvent( (Mes = new MessageEvent(event::TermResize) ) );
-        return Mes;
-    }
-    PostEvent( ( Kev = new NKeyPressEvent( e, bMeta ) ) );
-    bMeta = false;    /// @todo implement me
-    return  Kev;
-}
-
-
-/*!
-    \fn swMain::_prepare_message_ev(int nc )
- */
-Event* swMain::_prepare_message_ev(int nc )
-{
-    /// @todo implement me
-}
 
